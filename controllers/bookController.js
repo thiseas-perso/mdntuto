d955068de18d053a0f5222cc161176bf25202eb4
thiseaskambas <tkambas@yahoo.com>
2022-08-09 17:30:40 +0300

    start part5

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,4 +1,7 @@
 const Book = require('../models/book');
+const Author = require('../models/author');
+const Genre = require('../models/genre');
+const BookInstance = require('../models/bookInstance');
 
 exports.index = function (req, res) {
   res.send('NOT IMPLEMENTED: Site Home Page');
