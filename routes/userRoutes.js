b0263299f27f64800a021ab974122024ca73d6a7
thiseaskambas <tkambas@yahoo.com>
2022-08-09 13:16:57 +0300

    end of part2

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,4 +5,7 @@ const router = express.Router();
 router.get('/', (req, res) => {
   res.send('users get route');
 });
+router.get('/cool', (req, res) => {
+  res.render('index', { title: 'Cool', text: 'You are so cool' });
+});
 module.exports = router;
