7b9993d8169ba5d0bad91967536dee50d33f0731
thiseaskambas <tkambas@yahoo.com>
2022-08-09 16:25:58 +0300

    odin project msg board end

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,10 +2,31 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date(),
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date(),
+  },
+];
+
+router.route('/').get((req, res) => {
   res.send('users get route');
 });
-router.get('/cool', (req, res) => {
-  res.render('index', { title: 'Cool', text: 'You are so cool' });
-});
+
+router
+  .route('/cool')
+  .get((req, res) => {
+    res.render('cool', { title: 'Cool', messages });
+  })
+  .post((req, res) => {
+    const { text, user } = req.body;
+    messages.push({ text, user, added: new Date() });
+    res.redirect(req.originalUrl);
+  });
 module.exports = router;
